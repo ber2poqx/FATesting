b5e2fef0b5d8b8c4a2bd60e76534bcf6fb84adc6
spyrax10 <smarteman10@gmail.com>
2022-03-12 14:00:08 +0800

    Follow up fix on last commit

--- applications/inventory.php
+++ applications/inventory.php
@@ -79,9 +79,8 @@ class inventory_app extends application
 
 		//Added by spyrax10 10 Mar 2022
 		$this->add_rapp_function(2, _("Import Item Master (New)"),
-			"inventory/manage/item_upload.php", 'SA_ITEM', MENU_MAINTENANCE
+			"inventory/manage/item_upload.php", 'SA_ITEM', MENU_ENTRY
 		);
-
 		$this->add_rapp_function(2, _("Import Item Color Code"),
 			"inventory/manage/color_upload.php", 'SA_FORITEMCODE', MENU_MAINTENANCE
 		);
