d8304b2f2ed984e4b53ccb92029145a36c334419
spyrax10 <smarteman10@gmail.com>
2022-04-21 14:07:33 +0800

    Disable Import Item Master (New)

--- applications/inventory.php
+++ applications/inventory.php
@@ -78,9 +78,9 @@ class inventory_app extends application
 			"inventory/item_apsupport.php?mngtype=type", 'SA_ITMAPSUPPORT', MENU_MAINTENANCE);
 
 		//Added by spyrax10 10 Mar 2022
-		$this->add_lapp_function(2, _("Import Item Master (New)"),
-			"inventory/manage/item_upload.php", 'SA_ITEM_UPLOAD', MENU_MAINTENANCE
-		);
+		// $this->add_lapp_function(2, _("Import Item Master (New)"),
+		// 	"inventory/manage/item_upload.php", 'SA_ITEM_UPLOAD', MENU_MAINTENANCE
+		// );
 		$this->add_rapp_function(2, _("Import Item Color Code"),
 			"inventory/manage/color_upload.php", 'SA_FORITEMCODE', MENU_MAINTENANCE
 		);
