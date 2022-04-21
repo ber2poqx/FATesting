0b3549967ef2b4cdcb94244352313d65fad44e83
RobertDusal <dusalrobert@gmail.com>
2022-04-21 15:50:52 +0800

    Update import_items.php

--- modules/import_items/import_items.php
+++ modules/import_items/import_items.php
@@ -316,11 +316,11 @@ if (isset($_POST['import'])) {
 					//$code = preg_replace('/[\r\n]+/', '\n', $code); /*Replace Multiple Newline, Tab*/
 					//$code = preg_replace('/\s+/', ' ', $code); /*Replace Multiple Space*/
 
-                    $id = preg_replace('/[\r\n]+/', '\n', $id);
-					//$id = preg_replace('/[ \t]+/', ' ', $id);
+                    //$id = preg_replace('/[\r\n]+/', '\n', $id);
+					$id = preg_replace('/[ \t]+/', ' ', $id);
 
-					$code = preg_replace('/[\r\n]+/', '\n', $code);
-					//$code = preg_replace('/[ \t]+/', ' ', $code);
+					//$code = preg_replace('/[\r\n]+/', '\n', $code);
+					$code = preg_replace('/[ \t]+/', ' ', $code);
 
 			    $sql = "SELECT stock_id FROM ".TB_PREF."stock_master WHERE stock_id = ".db_escape($id);
 			    $result = db_query($sql,"item could not be retreived");
