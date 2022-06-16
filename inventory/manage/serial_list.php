9783d83528c48b5e63b889cefe68017b36e0d985
spyrax10 <smarteman10@gmail.com>
2022-06-16 15:27:39 +0800

    Address Mantis Issue #894

--- inventory/manage/serial_list.php
+++ inventory/manage/serial_list.php
@@ -23,35 +23,15 @@ if ($SysPrefs->use_popup_windows && $SysPrefs->use_popup_search) {
 page(_($help_context = "PNP Clearance Monitoring"), false, false, "", $js);
 //--------------------------------------------------------------------------------------------------
 
-function get_all_serial($search = '') {
-
-    global $db_connections;
-    $sql = "";
-    for ($i = 0; $i < count($db_connections); $i++) {
-        if (check_db_exists($i)) {
-            $branch_code = $db_connections[$i]['branch_code'];
-            $db_name = $db_connections[$i]['dbname'];
-
-            $sql .= "SELECT '$branch_code' AS branch, 
-                $branch_code.* 
-            FROM $db_name.item_serialise $branch_code";
-
-            if ($search != '') {
-                $sql .= " 
-                WHERE serialise_item_code LIKE " . db_escape('%' . trim($search) . '%') . " OR 
-				serialise_lot_no LIKE " . db_escape('%' . trim($search) . '%') . " OR 
-				serialise_chasis_no LIKE " . db_escape('%' . trim($search) . '%');
-            }
-
-            if ($i < count($db_connections) - 1) {
-                $sql .= " UNION ALL "; 
-            }
-        }
+if (get_post('comp_id')) {
+    $default_table_count = count_columns(0, 'item_serialise');
+    $company_check = get_post('comp_id') != null && check_db_exists(get_post('comp_id')) 
+        && $default_table_count == count_columns(get_post('comp_id'), 'item_serialise');
+    
+    if(!$company_check) {
+        display_error(_("No rows return in Selected Branch! Displaying all data..."));
     }
-
-    return db_query($sql, _("get_all_serial()"));
 }
-
 //--------------------------------------------------------------------------------------------------
 
 start_form(false, false, $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING']);
@@ -60,6 +40,7 @@ start_table(TABLESTYLE_NOBORDER);
 start_row();
 
 ref_cells(_("Search Here: &nbsp;"), 'searchval', '', null, '', true);
+company_list_row(_('&nbsp; Origin Branch: '), 'comp_id', true, true, false);
 
 end_row();
 end_table();
@@ -73,18 +54,25 @@ $Ajax->activate('item_tbl');
 end_row();
 end_table(); 
 
-$res_details = get_all_serial(get_post('searchval'));
+$res_details = get_all_serial(
+    get_post('searchval'), 
+    get_post('comp_id')
+);
 
 div_start('item_tbl');
-start_table(TABLESTYLE, "width='98%'");
+start_table(TABLESTYLE, "width='99%'");
 
 $th = array(
+    _("ID"),
     _("Origin Branch"),
     _("Category"),
     _("Item Code"),
-    _("Item Description"),
+    _("Color | Description"),
     _("Serial/Engine Number"),
-    _("Chassis Number")
+    _("Chassis Number"),
+    _("PNP Cleared"),
+    _("Note"),
+    _("")
 );
 
 table_header($th);
@@ -95,13 +83,21 @@ while ($row = db_fetch_assoc($res_details)) {
     alt_table_row_color($k);
 
     $stock_row = db_fetch_assoc(get_stock_by_itemCode($row['serialise_item_code']));
+    $is_cleared = $row['cleared'] == 1 ? _("Yes") : _("No");
 
+    label_cell($row['serialise_id']);
     label_cell(get_company_value(get_comp_id($row['branch']), 'name'));
     label_cell(get_category_name($stock_row['category_id']));
     label_cell($stock_row['stock_id']);
-    label_cell($stock_row['description']);
-    label_cell($row['serialise_lot_no']);
-    label_cell($row['serialise_chasis_no']);
+    label_cell($stock_row['color'] != '' ? $stock_row['color'] . " | " . 
+        get_color_description($row['serialise_item_code'], $stock_row['stock_id']) : 
+        get_color_description($row['serialise_item_code'], $stock_row['stock_id'])
+    );
+    label_cell($row['serialise_lot_no'], "nowrap");
+    label_cell($row['serialise_chasis_no'], "nowrap");
+    label_cell($is_cleared, "align='center'");
+    label_cell($row['pnp_note'], "nowrap");
+    label_cell(serial_update_cell(get_comp_id($row['branch']), $row['serialise_id']), "nowrap");
 }
 
 
---
+++ inventory/view/serial_update.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Added by: spyrax10
+ * Date Added: 16 Jun 2022
+*/
+
+$page_security = 'SA_FORITEMCODE';
+$path_to_root = "../..";
+
+include_once($path_to_root . "/includes/session.inc");
+include_once($path_to_root . "/includes/date_functions.inc");
+include_once($path_to_root . "/includes/ui.inc");
+include_once($path_to_root . "/includes/data_checks.inc");
+
+include_once($path_to_root . "/inventory/includes/inventory_db.inc");
+
+simple_page_mode(true);
+
+$js = "";
+if ($SysPrefs->use_popup_windows && $SysPrefs->use_popup_search) {
+    $js .= get_js_open_window(900, 300);
+}
+
+page(_($help_context = "Serial Update"), true, false, "", $js);
+
+//----------------------------------------------------------------------------------------------------
+function serial_pnp_update($company_id, $trans_no, $cleared = 0, $pnp_note = '') {
+    global $Ajax;
+
+    if ($company_id == -1) {
+		$company_id = $_SESSION["wa_current_user"]->company;
+	}
+
+    set_global_connection($company_id);
+
+    $sql = "UPDATE ".TB_PREF."item_serialise 
+        SET cleared = $cleared";
+    
+    if ($pnp_note != '') {
+        $sql .= ", pnp_note = '$pnp_note'";
+    }
+
+    $sql .= " WHERE serialise_id = " .db_escape($trans_no);
+
+    db_query($sql, "serial_pnp_update()");
+
+    $Ajax->activate('_page_body');
+    display_notification(_("Transaction ID #" . $trans_no . " sucessfully updated!"));
+}
+
+
+//----------------------------------------------------------------------------------------------------
+
+function display_details($company_id, $trans_no) {
+
+    $result = get_all_serial('', $company_id, $trans_no);
+
+    div_start('serial_head');
+    start_table(TABLESTYLE, "width='95%'");
+
+    $th = array(
+        _("ID"),
+        _("Origin Branch"),
+        _("Category"),
+        _("Item Code"),
+        _("Color | Description"),
+        _("Serial/Engine Number"),
+        _("Chassis Number"),
+        _("PNP Cleared")
+    );
+
+    table_header($th);
+
+    $k = 0;
+
+    while ($row = db_fetch($result)) {
+        alt_table_row_color($k);
+
+        $stock_row = db_fetch_assoc(get_stock_by_itemCode($row['serialise_item_code']));
+        $is_cleared = $row['cleared'] == 1 ? _("Yes") : _("No");
+    
+        label_cell($row['serialise_id']);
+        label_cell(get_company_value(get_comp_id($row['branch']), 'name'));
+        label_cell(get_category_name($stock_row['category_id']));
+        label_cell($stock_row['stock_id']);
+        label_cell($stock_row['color'] != '' ? $stock_row['color'] . " | " . 
+            get_color_description($row['serialise_item_code'], $stock_row['stock_id']) : 
+            get_color_description($row['serialise_item_code'], $stock_row['stock_id'])
+        );
+        label_cell($row['serialise_lot_no'], "nowrap");
+        label_cell($row['serialise_chasis_no'], "nowrap");
+        label_cell($is_cleared, "align='center'");
+        edit_button_cell("Edit".$row['serialise_id'], _("Edit"), _('Update line'));
+
+        start_row();
+        label_cells(_('Note: &nbsp;&nbsp;'), $row['pnp_note'], "class='tableheader2'", "colspan='7'");
+        end_row();
+    }
+
+    end_table();
+    div_end();
+}
+
+function display_edit_form($company_id, $status) {
+
+    $serial_row = db_fetch_assoc(get_serial_details($company_id, $status));
+
+    div_start("update_serial");
+    start_table(TABLESTYLE2);
+
+    check_row(_('PNP Cleared: '), 'cleared', null, false);
+    textarea_row(_("Note: "), 'memo_', null, 50, 3);
+
+    end_table(1);
+    div_end();
+    submit_add_or_update_center(false, '', 'both', false, false);
+}
+
+//----------------------------------------------------------------------------------------------------
+
+if (isset($_POST['UPDATE_ITEM'])) {
+    serial_pnp_update(
+        $_GET['coy'], 
+        $_GET['trans_no'], 
+        $_POST['cleared'] != null ? $_POST['cleared'] : 0, 
+        $_POST['memo_']
+    );
+}
+
+
+//----------------------------------------------------------------------------------------------------
+start_form(false, false, $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING']);
+
+
+display_details($_GET['coy'], $_GET['trans_no']);
+br();
+
+$edit_id = find_submit('Edit');
+if ($edit_id != -1) {
+    $id = get_post('selected_id', find_submit('Edit'));
+    display_edit_form($_GET['coy'], $id);
+}
+
+//----------------------------------------------------------------------------------------------------
+end_form();
+end_page(true);
