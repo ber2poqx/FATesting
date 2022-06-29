c830ea3aa9a59a0b2fb91bde67a685ab02c63220
spyrax10 <smarteman10@gmail.com>
2022-06-29 09:55:31 +0800

    Update fix on inventory.php

--- applications/inventory.php
+++ applications/inventory.php
@@ -78,7 +78,7 @@ class inventory_app extends application
 		$this->add_lapp_function(2, _("Inventory Adjustment Setup"),
 			"inventory/manage/inventory_setup.php?", 'SA_INVENTORY_TYPE', MENU_MAINTENANCE);
 
-		if ($_SESSION["wa_current_user"]->company == 0) {
+			if (get_company_value(user_company(), 'branch_code') == "HO" || get_company_value(user_company(), 'branch_code') == "DESIHOFC") {
 			$this->add_lapp_function(2, _("PNP Clearance Monitoring"),
 				"inventory/manage/serial_list.php", 'SA_FORITEMCODE', MENU_MAINTENANCE
 			);
@@ -95,7 +95,7 @@ class inventory_app extends application
 		// $this->add_lapp_function(2, _("Import Item Master (New)"),
 		// 	"inventory/manage/item_upload.php", 'SA_ITEM_UPLOAD', MENU_MAINTENANCE
 		// );
-		if ($_SESSION["wa_current_user"]->company == 0) {
+		if (get_company_value(user_company(), 'branch_code') == "HO" || get_company_value(user_company(), 'branch_code') == "DESIHOFC") {
 			$this->add_rapp_function(2, _("Import Item Color Code"),
 				"inventory/manage/color_upload.php", 'SA_FORITEMCODE', MENU_MAINTENANCE
 			);
