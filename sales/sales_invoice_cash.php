feea6fec9735c4e43bbc29380232b887add2b635
spyrax10 <smarteman10@gmail.com>
2022-07-26 16:35:07 +0800

    Update fix on SO Cash Discount

--- sales/sales_invoice_cash.php
+++ sales/sales_invoice_cash.php
@@ -414,7 +414,6 @@ if (get_post('StockLocation')) {
 }
 
 if (get_post('category_id')) {
-	clear_discount();
 	$Ajax->activate('order_head');
 }
 
@@ -708,8 +707,8 @@ function update_header() {
 
 		$total += $order_item->price * $order_item->qty_dispatched;
 
-		$dis1 += get_post('pdc_no') != '' ? floor($total * .05) : $order_item->discount1;
-		$dis2 += get_post('pdc_discount') == 1 ?  floor($total * .05) : $order_item->discount2;
+		$dis1 += get_post('pdc_no') != '' ? floor($order_item->price * $order_item->qty_dispatched * .05) : $order_item->discount1;
+		$dis2 += get_post('pdc_discount') == 1 ?  floor($order_item->price * $order_item->qty_dispatched * .05) : $order_item->discount2;
 
 		$total_lcp += $order_item->price * $order_item->qty_dispatched - ($dis1 + $dis2);
 
