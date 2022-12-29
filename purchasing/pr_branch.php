ba6320d0e7267351830080ea9f65ec78dcb5352f
albertpoqx <poqsbert@gmail.com>
2022-12-29 10:52:04 +0800

    pr_branch display warning

--- purchasing/pr_branch.php
+++ purchasing/pr_branch.php
@@ -505,6 +505,7 @@ start_table(TABLESTYLE_NOBORDER);
 start_row();
 // ahref(_("New Purchase Request"), "pr_entry_items.php?NewRequest=Yes");
 branch_company_list_row(_('Branch: '), 'selected_pr_branch', true, false, false);//modified by albert
+display_warning(get_post('selected_pr_branch'));
 
 ref_cells(_("PR#:"), 'pr_number', '', null, '', true);
 
