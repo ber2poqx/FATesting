566dfb2ba8bb42acba4918e9e67e7bf478589cd5
Prog6-des <Prog6-des@users.noreply.github.com>
2023-04-13 14:49:36 +0800

    Update rep723

--- reporting/rep723.php
+++ reporting/rep723.php
@@ -172,10 +172,13 @@ function print_SL_summary_particulars()
 
 	$orientation = 'P';
     $dec = user_price_dec();
-   
-    $account = get_GL_Title($gl_account);
-    $GL_title = db_fetch($account);
-    $account_name = $GL_title['account_name'];
+
+	if($gl_account != ALL_TEXT)
+	{
+		$account = get_GL_Title($gl_account);
+		$GL_title = db_fetch($account);
+		$account_name = $GL_title['account_name'];
+	}    
 	
     if($masterfile == ALL_TEXT)
     {
@@ -190,20 +193,39 @@ function print_SL_summary_particulars()
 	if($from == '')
 		$from = 0;
 	
-	if($from == 0)
+	if($gl_account == ALL_TEXT)
 	{
-		$params = array(0 => $comments,		
-		1 => array('text' => _('Date'),'from' => _('As of'), 'to' => $to),		
-		2 => array('text' => _('GL Title'), 'from' => $gl_account . _(' - ') . $account_name, 'to' => ''),
-		3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
-	}
-	if($from != 0)
+		if($from == 0)
+		{
+			$params = array(0 => $comments,		
+			1 => array('text' => _('Date'),'from' => _('As of'), 'to' => $to),		
+			2 => array('text' => _('GL Title'), 'from' => _('ALL') , 'to' => ''),
+			3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
+		}
+		if($from != 0)
+		{
+			$params = array(0 => $comments,		
+			1 => array('text' => _('Period'),'from' => $from, 'to' => $to),
+			2 => array('text' => _('GL Title'), 'from' => _('ALL') , 'to' => ''),
+			3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
+		}
+	}else
 	{
-	$params = array(0 => $comments,		
-		1 => array('text' => _('Period'),'from' => $from, 'to' => $to),
-		2 => array('text' => _('GL Title'), 'from' => $gl_account . _(' - ') . $account_name, 'to' => ''),
-		3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
-	}
+		if($from == 0)
+		{
+			$params = array(0 => $comments,		
+			1 => array('text' => _('Date'),'from' => _('As of'), 'to' => $to),		
+			2 => array('text' => _('GL Title'), 'from' => $gl_account . _(' - ') . $account_name, 'to' => ''),
+			3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
+		}
+		if($from != 0)
+		{
+			$params = array(0 => $comments,		
+			1 => array('text' => _('Period'),'from' => $from, 'to' => $to),
+			2 => array('text' => _('GL Title'), 'from' => $gl_account . _(' - ') . $account_name, 'to' => ''),
+			3 => array('text' => _('Masterfile Name'), 'from' => $Masterfile_name, 'to' => ''));
+		}
+	}	
 
 	$cols = array(5,   55,   90,  180,  280, 360,  	           435,   500,	0); 
 
@@ -231,40 +253,123 @@ function print_SL_summary_particulars()
     $rep->SetHeaderType('SL_Summary_Header');
 	$rep->NewPage();
 	
-	$res = getTransactions($from, $to, $gl_account, $masterfile);
-
-	if ($from != 0)
+	if($gl_account != ALL_TEXT)
 	{
-		$Forwarded_bal = getBalance_forwarded($from, $gl_account, $masterfile);
-	}
-
-	$Tot_bal = 0;
-	$Tot_deb = 0;
-	$Tot_cred = 0;
-	$running_bal = 0;
-	$amount_val = 0;
-	$Forwarded_deb = 0;
-	$Forwarded_cred = 0;
-
-	While ($SLsum = db_fetch($res))
-	{
-		if($running_bal == 0)
-		{	
-			$rep->NewLine(0.5);
-			$rep->Font('bold');
-			$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);
-			if($from == 0)
-			{
-				$rep->TextCol(6, 7, _('As of - ') . $to);
+		$res = getTransactions($from, $to, $gl_account, $masterfile);
+		
+		if ($from != 0)
+		{
+			$Forwarded_bal = getBalance_forwarded($from, $gl_account, $masterfile);
+		}
+	
+		$Tot_bal = 0;
+		$Tot_deb = 0;
+		$Tot_cred = 0;
+		$running_bal = 0;
+		$amount_val = 0;
+		$Forwarded_deb = 0;
+		$Forwarded_cred = 0;
+	
+		While ($SLsum = db_fetch($res))
+		{
+			if($running_bal == 0)
+			{	
+				$rep->NewLine(0.5);
+				$rep->Font('bold');
+				//$rep->Line($rep->row + 9, 0.5);
+				$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);
+				if($from == 0)
+				{
+					$rep->TextCol(6, 7, _('As of - ') . $to);
+				}
+				$rep->Line($rep->row - 2);
+				$rep->Font();
+	
+				if ($from != 0)
+				{
+					While ($F_bal = db_fetch($Forwarded_bal))
+					{	
+						$rep->Font('bold');
+						$rep->TextCol(4, 5, _('Balance Forwarded'));
+						$rep->AmountCol2(5, 6, $F_bal['Debit'], $dec);
+						$rep->AmountCol2(6, 7, $F_bal['Credit'], $dec);
+						$rep->AmountCol2(7, 8, $F_bal['Forwarded_Bal'], $dec);
+						$rep->Font();
+						$running_bal = $F_bal['Forwarded_Bal'];
+						$Forwarded_deb = $F_bal['Debit'];
+						$Forwarded_cred = $F_bal['Credit'];
+					}
+						
+				}										
 			}
-			$rep->Line($rep->row - 2);
-			$rep->Font();
-
-			if ($from != 0)
+	
+			$amount_val = $SLsum['amount'];
+	
+			//$running_bal = $running_bal + $amount_val;
+			$Tot_bal = $Tot_bal + $amount_val;
+	
+			if($amount_val >= 0)
 			{
+				$running_bal = $running_bal + $amount_val;
+			}
+			if ($amount_val < 0)
+			{
+				$amount_val = -$SLsum['amount'];
+				$running_bal = $running_bal - $amount_val;
+			}
+	
+			$dec2 = get_qty_dec($SLsum['reference']);
+	
+			$rep->NewLine();
+			$rep->TextCol(0, 1, $SLsum['tran_date']);
+			$rep->TextCol(1, 2, $SLsum['cr_num']);
+			$rep->TextCol(2, 3, $SLsum['reference']);
+			$rep->TextCol(3, 4, $SLsum['name']);
+			$rep->TextCol(4, 5, $SLsum['memo_']);
+			$rep->AmountCol2(5, 6, $SLsum['Debit'], $dec);
+			$rep->AmountCol2(6, 7, $SLsum['Credit'], $dec);
+			//$rep->AmountCol2(6, 7, -$running_bal, $dec);
+			
+			if ($running_bal < 0)
+				$rep->AmountCol2(7, 8, -$running_bal, $dec);
+			else
+				$rep->AmountCol2(7, 8, $running_bal, $dec);
+			
+	
+			$Tot_deb = $SLsum['Debit'] + $Tot_deb;
+			$Tot_cred = $SLsum['Credit'] + $Tot_cred;
+	
+	
+			
+	
+			// $rep->TextCol(5, 6, $GRNs['Name']);
+			// $rep->TextCol(6, 7, $GRNs['Model']);
+			// $rep->TextCol(7, 8, $GRNs['Serial']);
+			// $rep->TextCol(8, 9, $GRNs['Chassis']);
+			// $rep->TextCol(9, 10, $GRNs['Type']);
+			// $rep->TextCol(10, 11, $GRNs['Term']);
+			// $rep->TextCol(11, 12, $GRNs['Qty']);
+			// $rep->AmountCol2(12, 13, $GRNs['LCP']);
+			// $rep->AmountCol2(13, 14, $GRNs['UnitCost']);
+			// $rep->AmountCol2(14, 15, $GRNs['grossAmnt']);
+			// $rep->AmountCol2(15, 16, $GRNs['discountdp']);
+			// $rep->TextCol(16, 17, $GRNs['SalesAgent']);
+			
+			$rep->NewLine(0, 1);
+		}
+
+		if($Tot_bal != 0)
+		{
+			$rep->NewLine(1);
+			$rep->Line($rep->row + 10);
+		}
+			
+		if($Tot_bal == 0)
+		{
 				While ($F_bal = db_fetch($Forwarded_bal))
 				{	
 					$rep->Font('bold');
+					$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);
 					$rep->TextCol(4, 5, _('Balance Forwarded'));
 					$rep->AmountCol2(5, 6, $F_bal['Debit'], $dec);
 					$rep->AmountCol2(6, 7, $F_bal['Credit'], $dec);
@@ -273,126 +378,54 @@ function print_SL_summary_particulars()
 					$running_bal = $F_bal['Forwarded_Bal'];
 					$Forwarded_deb = $F_bal['Debit'];
 					$Forwarded_cred = $F_bal['Credit'];
-				}
-					
-			}										
+				}		
+			$rep->Line($rep->row);
+			$rep->NewLine(2);
+			$rep->TextCol(2, 7, _('-  -  -  -  -  -  -  No Transaction in the given Parameter  -  -  -  -  -  -  '));
+			$rep->NewLine(1);
 		}
-
-		$amount_val = $SLsum['amount'];
-
-		//$running_bal = $running_bal + $amount_val;
-		$Tot_bal = $Tot_bal + $amount_val;
-
-		if($amount_val >= 0)
-		{
-			$running_bal = $running_bal + $amount_val;
-		}
-		if ($amount_val < 0)
-		{
-			$amount_val = -$SLsum['amount'];
-			$running_bal = $running_bal - $amount_val;
-		}
-
-		$dec2 = get_qty_dec($SLsum['reference']);
-
-		$rep->NewLine();
-		$rep->TextCol(0, 1, $SLsum['tran_date']);
-		$rep->TextCol(1, 2, $SLsum['cr_num']);
-		$rep->TextCol(2, 3, $SLsum['reference']);
-		$rep->TextCol(3, 4, $SLsum['name']);
-		$rep->TextCol(4, 5, $SLsum['memo_']);
-		$rep->AmountCol2(5, 6, $SLsum['Debit'], $dec);
-		$rep->AmountCol2(6, 7, $SLsum['Credit'], $dec);
-		//$rep->AmountCol2(6, 7, -$running_bal, $dec);
 		
+		if($Tot_bal != 0)
+		{
+			$rep->Font('bold');
+			$rep->TextCol(4, 6, _('Subtotal'));
+			$rep->Font('italic');
+			$rep->AmountCol(5, 6, $Tot_deb, $dec);
+			$rep->AmountCol(6, 7, $Tot_cred, $dec);
+			$rep->Font();
+		}
+
+		$rep->Line($rep->row - 7);
+
+		$rep->NewLine(1.3);
+
+			
+		$rep->Font('bold');		
+		$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);	
+		$rep->TextCol(4, 5, _('Ending Balance'));
+		$rep->Font();		
+		$rep->Font('bold');
+		$Total1 = getEnding_bal($to, $gl_account, $masterfile);
+		While ($Total_amount = db_fetch($Total1))
+		{
+			$rep->AmountCol(5, 6, $Total_amount['Debit'], $dec);
+			$rep->AmountCol(6, 7, $Total_amount['Credit'], $dec);
+		}
 		if ($running_bal < 0)
-			$rep->AmountCol2(7, 8, -$running_bal, $dec);
-		else
-			$rep->AmountCol2(7, 8, $running_bal, $dec);
-		
+			$rep->AmountCol(7, 8, -$running_bal, $dec);
+		else		
+			$rep->AmountCol(7, 8, $running_bal, $dec);
+		$rep->Line($rep->row - 2, 0.5);	
 
-		$Tot_deb = $SLsum['Debit'] + $Tot_deb;
-		$Tot_cred = $SLsum['Credit'] + $Tot_cred;
-
-
-		
-
-		// $rep->TextCol(5, 6, $GRNs['Name']);
-		// $rep->TextCol(6, 7, $GRNs['Model']);
-		// $rep->TextCol(7, 8, $GRNs['Serial']);
-		// $rep->TextCol(8, 9, $GRNs['Chassis']);
-		// $rep->TextCol(9, 10, $GRNs['Type']);
-		// $rep->TextCol(10, 11, $GRNs['Term']);
-		// $rep->TextCol(11, 12, $GRNs['Qty']);
-		// $rep->AmountCol2(12, 13, $GRNs['LCP']);
-		// $rep->AmountCol2(13, 14, $GRNs['UnitCost']);
-		// $rep->AmountCol2(14, 15, $GRNs['grossAmnt']);
-		// $rep->AmountCol2(15, 16, $GRNs['discountdp']);
-		// $rep->TextCol(16, 17, $GRNs['SalesAgent']);
-		
-		$rep->NewLine(0, 1);
-	}
-
-	
-
-	if($Tot_bal != 0)
+	}	
+	else
 	{
-		$rep->NewLine(1);
-		$rep->Line($rep->row + 10);
-	}
-		
-	if($Tot_bal == 0)
-	{
-			While ($F_bal = db_fetch($Forwarded_bal))
-			{	
-				$rep->Font('bold');
-				$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);
-				$rep->TextCol(4, 5, _('Balance Forwarded'));
-				$rep->AmountCol2(5, 6, $F_bal['Debit'], $dec);
-				$rep->AmountCol2(6, 7, $F_bal['Credit'], $dec);
-				$rep->AmountCol2(7, 8, $F_bal['Forwarded_Bal'], $dec);
-				$rep->Font();
-				$running_bal = $F_bal['Forwarded_Bal'];
-				$Forwarded_deb = $F_bal['Debit'];
-				$Forwarded_cred = $F_bal['Credit'];
-			}		
 		$rep->Line($rep->row);
 		$rep->NewLine(2);
-		$rep->TextCol(2, 7, _('-  -  -  -  -  -  -  No Transaction in the given Parameter  -  -  -  -  -  -  '));
+		$rep->TextCol(2, 9, _('-  -  -  -  -  -  -  -  -  No Transaction in the given Parameter  -  -  -  -  -  -  -  -  '));
 		$rep->NewLine(1);
 	}
-	
-	if($Tot_bal != 0)
-	{
-		$rep->Font('bold');
-		$rep->TextCol(4, 6, _('Subtotal'));
-		$rep->Font('italic');
-		$rep->AmountCol(5, 6, $Tot_deb, $dec);
-		$rep->AmountCol(6, 7, $Tot_cred, $dec);
-		$rep->Font();
-	}
 
-	$rep->Line($rep->row - 25);
-
-	$rep->NewLine(3);
-
-		
-	$rep->Font('bold');		
-	$rep->TextCol(0, 4, $gl_account . _(' - ') . $account_name);	
-	$rep->TextCol(4, 5, _('Ending Balance'));
-	$rep->Font();		
-	$rep->Font('bold');
-	$Total1 = getEnding_bal($to, $gl_account, $masterfile);
-	While ($Total_amount = db_fetch($Total1))
-	{
-		$rep->AmountCol(5, 6, $Total_amount['Debit'], $dec);
-		$rep->AmountCol(6, 7, $Total_amount['Credit'], $dec);
-	}
-	if ($running_bal < 0)
-		$rep->AmountCol(7, 8, -$running_bal, $dec);
-	else		
-		$rep->AmountCol(7, 8, $running_bal, $dec);
-	$rep->Line($rep->row - 2);
 		
 	$rep->NewLine(2);
 
