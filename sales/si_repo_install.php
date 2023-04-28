e4e7a9bf783e5722f983f7a6cd459a76d46bbf22
albertpoqx <poqsbert@gmail.com>
2023-04-28 17:29:37 +0800

    Address change term Repo no. of months paid

--- sales/si_repo_install.php
+++ sales/si_repo_install.php
@@ -925,7 +925,7 @@ function new_installment_computation()
 		? $_POST['new_due_amort'] - $_POST['due_amort']
 		: $_POST['due_amort'] - $_POST['new_due_amort'];
 
-	$_POST['months_paid'] = count_months_paid_repo($_POST['document_ref']);
+	$_POST['months_paid'] = $terms;
 
 	$_POST['amort_delay'] = $_POST['new_due_amort'] > $_POST['due_amort']
 		? $_POST['amort_diff'] * $_POST['months_paid']
