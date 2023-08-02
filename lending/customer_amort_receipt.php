ab80d80a279fa0480ccda519b2840d2abd6de9e6
zhun10 <zhink_3roses@yahoo.com>
2023-08-02 15:22:33 +0800

    Update customer_amort_receipt.php

--- lending/customer_amort_receipt.php
+++ lending/customer_amort_receipt.php
@@ -2096,7 +2096,7 @@ if(isset($_GET['submitInterB']))
 
         if(!empty($company_prefs["deployment_status"])){
             interbranch_send_payment_add($_POST['branch_inb'], $interb_ref, $_POST['name_inb'], $trans_date, $_POST['ref_no_inb'], $_POST['total_amount_inb'],
-                                            $_POST['remarks_inb'], $_POST['preparedby_inb'], $conn['branch_code'], $payment_no, ST_CUSTPAYMENT, $_POST['branch_inb']);
+                                            $_POST['remarks_inb'], $_POST['preparedby_inb'], $conn['branch_code'], $payment_no, ST_CUSTPAYMENT, $_POST['branch_inb'], $_POST['moduletype_inb']);
 
             //make a copy to HO database
             //interbranch_send_payment_add("HO", $interb_ref, $_POST['name_inb'], $trans_date, $_POST['ref_no_inb'], $_POST['total_amount_inb'],
@@ -2106,7 +2106,7 @@ if(isset($_GET['submitInterB']))
 
             if(empty($company_prefs["deployment_status"]) == empty($islive)){
                 interbranch_send_payment_add($_POST['branch_inb'], $interb_ref, $_POST['name_inb'], $trans_date, $_POST['ref_no_inb'], $_POST['total_amount_inb'],
-                                            $_POST['remarks_inb'], $_POST['preparedby_inb'], $conn['branch_code'], $payment_no, ST_CUSTPAYMENT, $_POST['branch_inb']);
+                                            $_POST['remarks_inb'], $_POST['preparedby_inb'], $conn['branch_code'], $payment_no, ST_CUSTPAYMENT, $_POST['branch_inb'], $_POST['moduletype_inb']);
             }
         }
         //auto allocate payment para dili na makita sa customer allocation
